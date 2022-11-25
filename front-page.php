6e423b5a8e0db57d7582bc7c991ef3a6848c3f49
Bartek <bartek@byron.nl>
2022-11-25 16:42:45 +0100

    25 nov

--- front-page.php
+++ front-page.php
@@ -6,22 +6,33 @@
             <?php get_template_part('/template-parts/header_menu'); ?>
             <div class="sitetitle">
                 <div class="maintitle ">
-                    <h1>Textiel bedrukken op maat!</h1>
-                    <p class="subtext">Bestel hier uw custom prints!</p>
+                <h1> <?= get_field('main_page_title'); ?></h1>
+                <a href="#category" class="subtext"> 
+                    <?= get_field('main_page_text'); ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/> </svg>
+                </a>
                 </div>
             </div>
         </div>
     </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-8 text-center">
-            <div class="content">
-                <h1> <?= get_field('content_title'); ?></h1>
-                <p class="subtext"> <?= get_field('content_text'); ?></p>
-            </div>
-        </div>
+<div class="container image-container my-5">
+    <div class="row image-container justify-content-center" id="category">
+        <?php
+            if( have_rows('category_field') ):
+                while( have_rows('category_field') ) : the_row();
+                    $image = get_sub_field('category_image'); ?>
+                <div class="col-6 col-md-4 mb-3">
+                    <div class="image">
+                        <img class="category-image" src="<?= esc_url($image['sizes']["medium"]);  ?>" alt="">
+                        <div class="card-title">
+                            <?php $link = get_sub_field('category_title'); ?>
+                           <a href="<?php echo $link['url']?>"><?php echo $link['title']?></a>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile;?>
+       <?php endif;?>
     </div>
 </div>
-
 <?php get_footer(); ?>
--- template-parts/header_menu.php
+++ template-parts/header_menu.php
@@ -1,30 +1,32 @@
 <div class="container gx-0 header-menu">
-
         <div class="row d-flex m-0">
             <div class="col-12 px-0 m-0">
-            <div class="phone">
-                <a class="tel" href="tel:024 76 00 515 ">024 76 00 515</a>
+                <div class="phone">
+                    <a class="tel" href="tel:024 76 00 515 ">024 76 00 515</a>
                 </div>
             <nav class="navbar navbar-expand-lg" aria-label="navbar">
             <div class="container px-0">
                 <a class="navbar-brand" href="">Textiel drukkerij</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarcollapsemenu"
-                aria-controls="navbarcollapsemenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarcollapsemenu"
+                    aria-controls="navbarcollapsemenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <svg fill="var(--button-color)" class="hamburger" viewbox="0 0 100 100" width="50">
+                        <rect class="line top" width="80" height="10" x="10" y="25" rx="5"></rect>
+                        <rect class="line middle" width="80" height="10" x="10" y="45" rx="5"></rect>
+                        <rect class="line bottom" width="80" height="10" x="10" y="65" rx="5"></rect>
+                    </svg>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarcollapsemenu">
+            <div class="collapse navbar-collapse" id="navbarcollapsemenu">
                 <?php
-                        wp_nav_menu(
-                        array(
-                            'menu' => 'Main menu',
-                            'link_before' => '<span class="screen-reader-text">',
-                            'link_after' => '</span>',
-                        ));
+                    wp_nav_menu(
+                    array(
+                        'menu' => 'Main menu',
+                        'link_before' => '<span class="screen-reader-text">',
+                        'link_after' => '</span>',
+                    ));
                     ?>
                 </div>
             </div>
         </nav>
-            </div>
         </div>
     </div>
+</div>
