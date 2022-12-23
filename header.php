dae20aa012d09f2d13df1c141de108644c924ce8
Bartek <bartek@byron.nl>
2022-12-23 14:24:23 +0100

    xmas update 3

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- Template part voor de header -->
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,10 +8,13 @@
   <meta name="description" content="project">
   <meta name="Author" content="Bartek Filik">
   <title>project</title>
+  <!-- wp head functie wordt aangeroepen 
+  * Prints scripts or data in the head tag on the front end.
+  -->
   <?php
     wp_head()
     ?>
 </head>
-
+<!-- body krijgt altijd een class voor een specifiek template -->
 <body <?php body_class(); ?>>
     
