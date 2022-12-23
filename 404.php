6dbfa5fc8b4bba4688f10e39c979b5673ba7e069
Bartek <bartek@byron.nl>
2022-12-23 14:22:12 +0100

    xmas update 2

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<?php get_template_part('/template-parts/header_menu');?>
+<!-- 404 pagina -->
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>404, pagina niet gevonden</h1>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
