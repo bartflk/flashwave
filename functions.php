a0196f98cfe0dea72577ac10ef8318f915e4a891
Bartek <bartek@byron.nl>
2023-01-18 09:09:05 +0100

    merge

--- functions.php
+++ functions.php
@@ -7,49 +7,25 @@
  */ 
 
 if ( ! function_exists( 'myfirsttheme_setup' ) ) :
-    /**
-     * Sets up theme defaults and registers support for various WordPress features.
-     *
-     * Note that this function is hooked into the after_setup_theme hook, which runs
-     * before the init hook. The init hook is too late for some features, such as indicating
-     * support post thumbnails.
-     */
     function myfirsttheme_setup() {
-    
-        /**
-         * Make theme available for translation.
-         * Translations can be placed in the /languages/ directory.
-         */
         load_theme_textdomain( 'myfirsttheme', get_template_directory() . '/languages' );
-    
-        /**
-         * Add default posts and comments RSS feed links to &lt;head&gt;.
-         */
         add_theme_support( 'automatic-feed-links' );
-    
-        /**
-         * Enable support for post thumbnails and featured images.
-         */
         add_theme_support( 'post-thumbnails' );
-        /**
-         * Enable support for the following post formats:
-         * aside, gallery, quote, image, and video
-         */
         add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
     }
-    endif; // myfirsttheme_setup
+    endif;
     add_action( 'after_setup_theme', 'myfirsttheme_setup' );
 
 
 
-  // support voor woocommerce
+  // support voor woocommerce plugin
     add_action( 'after_setup_theme', 'woocommerce_support' );
     function woocommerce_support() {
        add_theme_support( 'woocommerce' );
     }                               
   
   add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
-  // geen woocommerce css bestanden laden
+  // dit zorgt dat geen woocommerce css bestanden laden
   add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
 
@@ -76,9 +52,8 @@ function register_my_menus() {
  add_theme_support( 'post-thumbnails', array( 'page' ) );  // Pages only
  add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
  add_theme_support( 'post-thumbnails' ); 
-
-  // Hooking up our function to theme setup
-  add_action( 'init', 'create_posttype' );
+ //een nieuw post type maken
+ add_action( 'init', 'create_posttype' );
   
   function add_theme_scripts() {
 // Alle CSS en Scripts worden hier ingeladen en neergezet
@@ -88,9 +63,7 @@ function register_my_menus() {
     wp_enqueue_script( 'bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' );
     wp_enqueue_style( 'flashwave', get_template_directory_uri() . '/assets/css/flashwave.css', array(), '');  
     wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/script.js', array (), '' , true );
-    
 
-    
       if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
       }
@@ -115,14 +88,11 @@ function register_my_menus() {
         'redirect'      => false
 ));
 }
-
-
-// To change add to cart text on single product page
+// Dit filter zorgt dat de add to car text in nederlands koop nu zegt
 add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
 function woocommerce_custom_single_add_to_cart_text() {
     return __( 'Koop nu', 'woocommerce' ); 
 }
-
 // To change add to cart text on product archives(Collection) page
 add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
 function woocommerce_custom_product_add_to_cart_text() {
